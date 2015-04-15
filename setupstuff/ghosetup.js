e780ca2fbeadd0f7a6633b5e426ce937e0e49878
Katharine Daly <dalyk@mit.edu>
2015-04-15 15:50:38 -0400

    changed styling of setup screens.  removed running distance screen

--- setupstuff/ghosetup.js
+++ setupstuff/ghosetup.js
@@ -19,87 +19,82 @@ $.extend({
     });
 
 $(window).on('hashchange', function() {
-if($.getHashVar('split') != null) {
-	$("#splitinput").hide();
-	$("#nosplitinput").hide();
-	$("#distanceinput").hide();
-	$("#nodistanceinput").hide();
-	$("#paceinput").show();
-	$("#nopaceinput").show();
-	$("#nexttext").hide();
-	$("#ghotext").show();
-	$("#nopacebtn").addClass("buttonselect");
-	$("#pacebtn").removeClass("buttonselect");
-	$("#pace")[0].value="";
-} else if ($.getHashVar('dist') != null) {
-	$("#distanceinput").hide();
-	$("#nodistanceinput").hide();
-	$("#paceinput").hide();
-	$("#nopaceinput").hide();
-	$("#splitinput").show();
-	$("#nosplitinput").show();
-	$("#nexttext").hide();
-	$("#ghotext").show();
-	$("#nosplitbtn").addClass("buttonselect");
-	$("#splitbtn").removeClass("buttonselect");
-	$("#split")[0].value="";
-} else {
-	$("#splitinput").hide();
-	$("#nosplitinput").hide();
-	$("#paceinput").hide();
-	$("#nopaceinput").hide();
-	$("#distanceinput").show();
-	$("#nodistanceinput").show();
-	$("#nexttext").show();
-	$("#ghotext").hide();
-	$("#nodistancebtn").addClass("buttonselect");
-	$("#distancebtn").removeClass("buttonselect");
-	$("#distance")[0].value="";
-}
-return false;
+	if($.getHashVar('split') != null) {
+		$("#splitinput").hide();
+		$("#nosplitinput").hide();
+		$("#paceinput").show();
+		$("#nopaceinput").show();
+		$("#nexttext").hide();
+		$("#ghotext").show();
+		//$("#nopacebtn").addClass("buttonselect");
+		//$("#pacebtn").removeClass("buttonselect");
+		$("#pace")[0].value="";
+	} else {
+		$("#splitinput").show();
+		$("#nosplitinput").show();
+		$("#paceinput").hide();
+		$("#nopaceinput").hide();
+		$("#nexttext").hide();
+		$("#ghotext").show();
+		//$("#nosplitbtn").addClass("buttonselect");
+		//$("#splitbtn").removeClass("buttonselect");
+		$("#split")[0].value="";
+	}
+	return false;
 });
 
 $(window).trigger('hashchange');
 
-
 $("#backbtn").on('click', function(){
-if($.getHashVar('split') != null){
-	location.hash="dist="+$.getHashVar('dist');
-} else if ($.getHashVar('dist') != null){
-	location.hash="";
-} else {
-	location.href="../index.html"
-}
-
+	if ($.getHashVar('split') != null){
+		location.hash="";
+	} 
+	else {
+		location.href="../index.html"
+	}
 });
 
 var stopPageInfo = "&type=save&run=0";
 
 $("#forwardbtn").on('click', function(){
-if($.getHashVar('split') != null){
-	if($("#pacebtn").hasClass("buttonselect")){
-		location.href="../running.html"+location.hash+"&pace="+$("#pace")[0].value+stopPageInfo;
-	} else {
-		location.href="../running.html"+location.hash+"&pace="+stopPageInfo;
+	if ($.getHashVar('split') != null) {
+		if ($('#pacebtn').hasClass('buttonselect')){
+			location.href="../running.html"+location.hash+"&pace="+$("#pace")[0].value+stopPageInfo;
+		} 
+		else {
+			location.href="../running.html"+location.hash+"&pace="+stopPageInfo;
+		}
 	}
-} else if($.getHashVar('dist') != null){
-	if($("#splitbtn").hasClass("buttonselect")){
-		location.hash+="&split="+$("#split")[0].value;
-	} else {
-		location.href="../running.html"+location.hash+"&split=&pace="+stopPageInfo;
+	else {
+		if ($('#splitbtn').hasClass('buttonselect')) {
+			location.hash+="split="+$("#split")[0].value;
+		}
+		else {
+			location.href="../running.html"+location.hash+"?split=&pace="+stopPageInfo;
+		}
 	}
-} else {
-	if($("#distancebtn").hasClass("buttonselect")){
-		location.hash+="dist="+$("#distance")[0].value;
-	} else {
-		location.hash+="dist=";
-	}
-}
 
-});
-$("#nodistancebtn").on('click', function(){
-	$("#nodistancebtn").addClass("buttonselect");
-	$("#distancebtn").removeClass("buttonselect");
+/*
+	if($.getHashVar('split') != null){
+		if($("#pacebtn").hasClass("buttonselect")){
+			location.href="../running.html"+location.hash+"&pace="+$("#pace")[0].value+stopPageInfo;
+		} else {
+			location.href="../running.html"+location.hash+"&pace="+stopPageInfo;
+		}
+	} else if($.getHashVar('dist') != null){
+		if($("#splitbtn").hasClass("buttonselect")){
+			location.hash+="&split="+$("#split")[0].value;
+		} else {
+			location.href="../running.html"+location.hash+"split=&pace="+stopPageInfo;
+		}
+	} else {
+		if($("#distancebtn").hasClass("buttonselect")){
+			location.hash+="dist="+$("#distance")[0].value;
+		} else {
+			location.hash+="dist=";
+		}
+	}
+	*/
 });
 
 $("#nosplitbtn").on('click', function(){
@@ -113,36 +108,6 @@ $("#nopacebtn").on('click', function(){
 	$("#nopacebtn").addClass("buttonselect");
 	$("#pacebtn").removeClass("buttonselect");
 });
-
-$("#distancebtn").on('click',function(){
-	$("#nodistancebtn").removeClass("buttonselect");
-	$("#distancebtn").addClass("buttonselect");
-	var totalmiles = {};
-	var totaldivisions = {}
-	for (var i = 0; i<31; i++){
-		totalmiles[i] = i;
-	}
-	for (var j = 0; j<100; j += 5){
-		if (j<10){
-			totaldivisions[j] = '0'+j;
-		} else {
-			totaldivisions[j]=j;
-		}
-	}
-	SpinningWheel.addSlot(totalmiles, 'right');
-	SpinningWheel.addSlot({separator: '.'}, 'readonly shrink');
-	SpinningWheel.addSlot(totaldivisions, 'left');
-	
-	SpinningWheel.setDoneAction(distanceDone);
-
-	SpinningWheel.open();
-});
-
-
-function distanceDone() {
-	$("#distance")[0].value = SpinningWheel.getSelectedValues().values.join().replace(/,/g, '');
-	$("#forwardbtn").focus();
-}
 
 $("#splitbtn").on('click',function(){
 	$("#nosplitbtn").removeClass("buttonselect");
@@ -174,7 +139,7 @@ $("#splitbtn").on('click',function(){
 
 function splitDone() {
 	$("#split")[0].value = SpinningWheel.getSelectedValues().values.join().replace(/,/g, '');
-	$("#forwardbtn").focus();
+	//$("#forwardbtn").focus();
 }
 
 $("#pacebtn").on('click',function(){
@@ -206,9 +171,14 @@ $("#pacebtn").on('click',function(){
 
 function paceDone(){
 	$("#pace")[0].value = SpinningWheel.getSelectedValues().values.join().replace(/,/g, '');
-	$("#forwardbtn").focus();
+	//$("#forwardbtn").focus();
 }
 
+$(":button").css("height", $(window).height()/12);
 
+//var offset = $("#backbtn").offset().left;
+//$("#backbtn").css("bottom", offset);
+//$("#forwardbtn").css("bottom", offset);
+//$("#forwardbtn").css("right", offset);
 
 });
